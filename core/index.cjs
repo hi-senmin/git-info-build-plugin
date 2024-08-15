8a178e1e1c19b6e90bee45608f8b06086a22f00c
senmin <senmin1208@163.com>
2024-08-15 17:26:37 +0800

    feat(): 新增配置 outFile、buildTime

--- core/index.cjs
+++ core/index.cjs
@@ -1,6 +1,8 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable max-classes-per-file */
 const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
 
 // eslint-disable-next-line no-underscore-dangle
 function getOptions(op = {}) {
@@ -13,11 +15,13 @@ function getOptions(op = {}) {
     globalVarName: '__PROJECT_VERSION_INFO__',
     console: true,
 
+    outFile: '_version.txt',
+    buildTime: true,
     ...op,
   };
 }
 
-function getGitBranchAndCommit(hash, time, branch, tag) {
+function getGitBranchAndCommit({ hash, time, branch, tag, buildTime: bTime }) {
   try {
     const commitHash = hash ? execSync('git rev-parse HEAD').toString().trim() : undefined;
     const commitTime = time ? execSync(`git log -1 --format='%ci'`).toString().trim() : undefined;
@@ -27,12 +31,14 @@ function getGitBranchAndCommit(hash, time, branch, tag) {
       : undefined;
 
     const gitTag = tag ? execSync('git tag --points-at HEAD').toString().trim() : undefined;
+    const buildTime = bTime ? new Date().toLocaleString() : undefined;
 
     const data = JSON.stringify({
       commitHash,
       commitTime,
       gitBranch,
       gitTag,
+      buildTime,
     });
     return data;
   } catch (error) {
@@ -58,11 +64,12 @@ class WebpackPluginGitInfoInject {
   }
 
   setInjectedHtml(compilation) {
-    const { hash, time, branch, tag, htmlFile, globalVarName, console } = this.options;
+    const { hash, time, branch, tag, htmlFile, globalVarName, console, outFile, buildTime } =
+      this.options;
     // 找到并修改index.html文件
     const htmlContent = compilation.assets[htmlFile].source();
 
-    const info = getGitBranchAndCommit(hash, time, branch, tag);
+    const info = getGitBranchAndCommit({ hash, time, branch, tag, buildTime });
 
     const injectedHtml = `${htmlContent} <script>${getScriptCode(
       globalVarName,
@@ -71,10 +78,18 @@ class WebpackPluginGitInfoInject {
     )}</script>`;
     // 更新assets中的index.html
     // eslint-disable-next-line no-param-reassign
-    compilation.assets['index.html'] = {
+    compilation.assets[htmlFile] = {
       source: () => injectedHtml,
       size: () => injectedHtml.length,
     };
+
+    if (outFile) {
+      // eslint-disable-next-line no-param-reassign
+      compilation.assets[outFile] = {
+        source: () => info,
+        size: () => info.length,
+      };
+    }
     return injectedHtml;
   }
 
@@ -105,13 +120,25 @@ class Webpack3PluginGitInfoInject extends WebpackPluginGitInfoInject {
 }
 
 function vitePluginGitInfoInject(options = {}) {
-  const { hash, time, branch, tag, globalVarName, console } = getOptions(options);
+  const { hash, time, branch, tag, globalVarName, console, outFile, buildTime } =
+    getOptions(options);
+  let outputDir; // 保存输出目录路径
+
+  let info = '';
+  let code = '';
 
   return {
     name: 'vite-plugin-git-info-inject',
+    // 获取 Vite 配置
+    configResolved(resolvedConfig) {
+      // 获取输出目录和项目根路径
+      outputDir = resolvedConfig.build.outDir;
+
+      info = getGitBranchAndCommit({ hash, time, branch, tag, buildTime });
+      code = getScriptCode(globalVarName, info, console);
+    },
+
     transformIndexHtml(html) {
-      const info = getGitBranchAndCommit(hash, time, branch, tag);
-      const code = getScriptCode(globalVarName, info, console);
       if (info) {
         return [
           {
@@ -122,6 +149,16 @@ function vitePluginGitInfoInject(options = {}) {
         ];
       }
       return html;
+    },
+    closeBundle() {
+      try {
+        if (outFile) {
+          const filePath = path.join(outputDir, outFile);
+          fs.writeFileSync(filePath, info);
+        }
+      } catch (error) {
+        console.error(error);
+      }
     },
   };
 }
